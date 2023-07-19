ff17f8842baa6e2fc3fa75e7dc6f92e31524f714
SWARAAT CHATTERJEE <swaraatchatterjee@gmail.com>
2023-07-19 16:19:12 +0530

    game over case partially solve

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+js:
 document.onkeydown = function (e) {
     console.log("key code is: ", e.keyCode);
 
@@ -6,7 +7,28 @@ document.onkeydown = function (e) {
         meu.classList.add("animatemeu");
         setTimeout(() => {
             meu.classList.remove("animatemeu");
-        }, 700);
+        }, 500);
     }
 
 }
+
+setInterval(() => {
+        meu=document.querySelector(".meu");
+        gameover=document.querySelector(".gameover");
+        obstacle=document.querySelector(".obstacle");
+
+        
+        dx=parseInt(window.getComputedStyle(meu,null).getPropertyValue("left"));
+        dy=parseInt(window.getComputedStyle(meu,null).getPropertyValue("top"));
+        ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("left"));
+        oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("top"));
+
+        ofsetX= Math.abs(dx-ox);
+        offsetY=Math.abs(dy-oy);
+
+        if(ofsetX<95 && offsetY<52)
+        {
+            gameover.style.visibility="visible";
+            obstacle.classList.remove("obsticalhero");
+        }
+}, 100);
