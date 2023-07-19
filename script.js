b0217bbd4c193466f12e226151425056e438672b
SWARAAT CHATTERJEE <swaraatchatterjee@gmail.com>
2023-07-20 00:44:19 +0530

    without music the project is made

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-js:
+score = 0;
+cross = true;
 document.onkeydown = function (e) {
     console.log("key code is: ", e.keyCode);
 
@@ -14,26 +15,47 @@ document.onkeydown = function (e) {
         muex = parseInt(window.getComputedStyle(meu, null).getPropertyValue("left"));
         meu.style.left = muex + 112 + "px"; // Fix the character movement to the right
     }
-
+    if (e.keyCode == 37) {
+        meu = document.querySelector(".meu");
+        muex = parseInt(window.getComputedStyle(meu, null).getPropertyValue("left"));
+        meu.style.left = (muex - 90) + "px"; // Fix the character movement to the left
+    }
 }
 
 setInterval(() => {
-        meu=document.querySelector(".meu");
-        gameover=document.querySelector(".gameover");
-        obstacle=document.querySelector(".obstacle");
+    meu = document.querySelector(".meu");
+    gameover = document.querySelector(".gameover");
+    obstacle = document.querySelector(".obstacle");
 
-        
-        dx=parseInt(window.getComputedStyle(meu,null).getPropertyValue("left"));
-        dy=parseInt(window.getComputedStyle(meu,null).getPropertyValue("top"));
-        ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("left"));
-        oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("top"));
+    dx = parseInt(window.getComputedStyle(meu, null).getPropertyValue("left"));
+    dy = parseInt(window.getComputedStyle(meu, null).getPropertyValue("top"));
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue("left"));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue("top"));
 
-        ofsetX= Math.abs(dx-ox);
-        offsetY=Math.abs(dy-oy);
+    ofsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
 
-        if(ofsetX<95 && offsetY<40)
-        {
-            gameover.style.visibility="visible";
-            obstacle.classList.remove("obsticalhero");
-        }
-}, 100);
+    if (ofsetX < 80 && offsetY < 40) {
+        gameover.style.visibility = "visible";
+        obstacle.classList.remove("obsticalhero");
+
+    }
+    else if (ofsetX < 145 && cross) {
+        score = score + 1;
+        updatescore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.3;
+            obstacle.style.animationDuration = newDur + "s";
+        }, 500)
+
+    }
+}, 10);
+
+function updatescore(score) {
+    scorecount.innerHTML = "Your Score: " + score;
+}
